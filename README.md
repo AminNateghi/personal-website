0c111d9243f655d940cbe503c218959f3f9f9df0
Amin Nateghi <nateghixyz@gmail.com>
2025-06-28 14:05:41 -0400

    init project

---
+++ README.md
@@ -0,0 +1,26 @@
+# Amin Nateghi - Personal Website
+
+## Project Structure
+
+Inside of my site, you'll see the following folders and files:
+
+```text
+/
+├── public/
+├── src/
+├── └── assets/
+├── └── layouts/
+│   └── pages/
+│       └── index.astro
+└── package.json
+```
+
+## Commands
+
+All commands are run from the root of the project, from a terminal:
+
+| Command        | Action                                      |
+| :------------- | :------------------------------------------ |
+| `pnpm install` | Installs dependencies                       |
+| `pnpm dev`     | Starts local dev server at `localhost:4321` |
+| `pnpm build`   | Build site to `./dist/`                     |
---
+++ src/assets/scripts/modal.js
@@ -0,0 +1,30 @@
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-project-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-image]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+// add click event to all modal items
+for (let i = 0; i < testimonialsItem.length; i++) {
+  testimonialsItem[i].addEventListener("click", function () {
+    modalImg.src = this.querySelector("[data-project-image]").src;
+    modalImg.alt = this.querySelector("[data-project-image]").alt;
+
+    testimonialsModalFunc();
+  });
+}
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
