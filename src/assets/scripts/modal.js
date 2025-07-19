570fc37609bbac1a262c3a3bd7c7c27e83159311
Amin Nateghi <nateghixyz@gmail.com>
2025-07-19 04:18:05 -0400

    refactor: change routing

--- src/assets/scripts/modal.js
+++ src/assets/scripts/modal.js
@@ -1,5 +1,5 @@
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-project-item]");
+// variables
+const projectItem = document.querySelectorAll("[data-project-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -10,21 +10,21 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const projectModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 };
 
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
-  testimonialsItem[i].addEventListener("click", function () {
+for (let i = 0; i < projectItem.length; i++) {
+  projectItem[i].addEventListener("click", function () {
     modalImg.src = this.querySelector("[data-project-image]").src;
     modalImg.alt = this.querySelector("[data-project-image]").alt;
 
-    testimonialsModalFunc();
+    projectModalFunc();
   });
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+modalCloseBtn.addEventListener("click", projectModalFunc);
+overlay.addEventListener("click", projectModalFunc);
